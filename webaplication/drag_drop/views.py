d0ff978b8e7e5d34237235ccdb1e589a7927db92
Julio Cesar G.E <juliogalvez90@gmail.com>
2022-09-12 21:20:28 -0500

    Se incluye carga de archivos en repositorio 'media/' de la app drag_drop

--- webaplication/drag_drop/views.py
+++ webaplication/drag_drop/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 from .processfiles import handle_uploaded_file #Function for file process
 from django.core.files.storage import FileSystemStorage
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -10,12 +11,16 @@ def index(request):
 
 
 def upload_file(request):
-    #if request.method == 'POST' and request.FILES['myfile']:
-    #    myfile = request.FILES['myfile']
-    #    fs = FileSystemStorage()
-    #    filename = fs.save(myfile.name, myfile)
-    #    uploaded_file_url = fs.url(filename)
-    #    return render(request, 'drag_drop/loadfileform.html', { 'uploaded_file_url': uploaded_file_url })
-    print("Impresion desde Upload_files. Super!!!")
+    if request.method == 'POST' and request.FILES['First_File']:
+        print(request.FILES)
+        myfile = request.FILES['First_File']
+        print("Nombre del archivo cargado: {}".format(myfile.name))
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        
+        messages.success(request, 'El archivo fue cargado en la ruta: drag_drop/media{}'.format(uploaded_file_url))
+        return render(request, 'drag_drop/loadfileform.html', { 'uploaded_file_url': uploaded_file_url })
+    
     return render(request, 'drag_drop/loadfileform.html')
 
